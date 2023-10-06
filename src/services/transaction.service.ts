cd8e6e47745e7060bedec0d39909d7f3717ff271
amrohan <amrohanx@gmail.com>
2023-10-06 21:15:47 +0530

    removeed unuse code

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable, catchError } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 import { transactionModel } from 'src/models/transaction';
 
 @Injectable({
@@ -16,23 +16,6 @@ export class TransactionService {
   // http://localhost:3000/budgets/month?userId=auth0|6502b89367e9a06d1933748d&monthName=Sep&year=2023
 
 
-  GetPerticularMonthTransactions(userId: string, month: string, year: string): Observable<transactionModel> {
-
-    let params = new HttpParams()
-      .set('userId', userId)
-      .set('monthName', month)
-      .set('year', year);
-
-    return this.http.get<transactionModel>(`${this.apiUrl}/budgets/month`, {
-      headers: { Authorization: `Bearer ${environment.transactionApi.AuthToken}` },
-      params: params,
-    }).pipe(
-      catchError((error) => {
-        console.error('There was an error!', error);
-        throw error;
-      })
-    );
-  }
 
 
 }
