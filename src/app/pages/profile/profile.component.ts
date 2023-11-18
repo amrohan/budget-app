c32a84a566a39c5c54207bdfed91bf8fef1f509c
amrohan <rohanxsalunkhe@gmail.com>
2023-11-18 20:26:22 +0530

    add: no records placeholder & profile page

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { AuthService } from '@auth0/auth0-angular';
 @Component({
   selector: 'app-profile',
-
   templateUrl: './profile.component.html',
 })
 export class ProfileComponent {
+
+  profileJson: string;
+
+
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) { }
+
+
 
 }
